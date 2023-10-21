9ee57484e5a29ee763ddd9d1962f164047f73dae
Anton Dyrdin <a.dyrdin@runetsoft.ru>
2023-10-21 03:42:34 +0300

    rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_213957) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_08_25_213957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ckeditor_content", force: :cascade do |t|
-    t.string "html", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "ckeditor_contents", force: :cascade do |t|
     t.string "html", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
 end
