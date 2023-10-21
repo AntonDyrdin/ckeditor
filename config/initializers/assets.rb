1898ab2bac7e4b3057d57b059be93ccd1d5e4249
Anton Dyrdin <a.dyrdin@runetsoft.ru>
2023-10-22 01:11:47 +0300

    фикс ассетов

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,10 +4,10 @@
 Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('app/javascript')
+Rails.application.config.assets.paths << Rails.root.join('app/assets/javascripts')
+Rails.application.config.assets.paths << Rails.root.join('app/assets/stylesheets')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile = ['*.js', '*.css', '**/*.js', '**/*.css']
